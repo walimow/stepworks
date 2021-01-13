16b4f31e5da4d1999d8f03f283fc1e4f15ebb7f7
walimow <walim@gmx.net>
2021-01-13 23:29:24 +0100

    c2c //->(box::type_morph)

---
+++ include/box/typemorph.hpp
@@ -0,0 +1,18 @@
+//
+// Created by kh on 13.01.21.
+#pragma once
+
+#include <box/box.hpp>
+
+namespace stepworks::bxx{
+
+    template <typename Ta, typename Tb, template <typename...> typename Src,
+                template <typename...> typename Dest=Src>
+            struct  morph{
+
+                //using fab=
+
+            };
+
+
+}
